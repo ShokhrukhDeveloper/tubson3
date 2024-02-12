f1e466a329e0829c8fd542c17cf2ff17c56eab18
Shoxrux <shoxruxyarashovuz@gmail.com>
2024-02-12 21:26:05 +0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+System.Console.WriteLine("Son kiritinng: ");
+string userInput = System.Console.ReadLine();
+int son = Convert.ToInt32(userInput);
+int i = son;
+int tubson=0;
+
+while (true)
+{   
+    if (tubson>=3)
+    {
+        break;
+    }
+
+    bool tub = true;
+    for (int j = 2; j <= Math.Sqrt(i); j++)
+      {
+        if (i % j == 0)
+        {
+            tub = false;
+            break;
+        }
+      }
+      if (tub)
+      {
+        System.Console.WriteLine(i);
+        tubson++;
+      }
+      i++;
+}
+      
+
